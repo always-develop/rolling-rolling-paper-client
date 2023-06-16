ba4b1bbfb331a49bfbdeb7032d3c68777485145e
appleSimple <jy_4148@naver.com>
2023-06-17 01:14:53 +0900

    feat: customize input component

--- src/components/form/Input.tsx
+++ src/components/form/Input.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import 'assets/styles/form/Input.scss';
 
 type InputProps = {
   id: string;
   placeholder?: string;
+  labelName?: string;
 };
 
 function Input(props: InputProps) {
   return (
-    <input id={props.id} type="text" placeholder={props.placeholder}></input>
+    <div className="text-form-group">
+      <label htmlFor="{props.id}">{props.labelName}</label>
+      <input id={props.id} type="text" placeholder={props.placeholder}></input>
+    </div>
   );
 }
 
