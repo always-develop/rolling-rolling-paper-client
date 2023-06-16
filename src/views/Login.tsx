810a670e1fae1707d9f8a1c1d54f9d3e06ea0001
appleSimple <jy_4148@naver.com>
2023-06-17 01:13:43 +0900

    feat : add login form

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import 'assets/styles/views/Home.scss';
 import Button from 'components/form/Button';
+import Input from 'components/form/Input';
 
 function Login() {
-  function goLoginPage() {
+  function postLogin() {
     console.log('ddd');
   }
 
   return (
     <div className="home-wrapper">
-      <Button name="로그인" onButtonClick={goLoginPage} />
+      <Input
+        id="loginId"
+        labelName="아이디"
+        placeholder="아이디를 입력해주세요."
+      />
+      <Input
+        id="loginPassword"
+        labelName="비밀번호"
+        placeholder="비밀번호를 입력해주세요."
+      />
+      <Button name="로그인" onButtonClick={postLogin} />
     </div>
   );
 }
