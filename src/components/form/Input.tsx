fb23df67c1d0e8a13bb93362375f15f40a924c7d
appleSimple <jy_4148@naver.com>
2023-06-18 14:19:20 +0900

    feat : making input component

--- src/components/form/Input.tsx
+++ src/components/form/Input.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { forwardRef, useRef } from 'react';
 import 'assets/styles/form/Input.scss';
 
 type InputProps = {
@@ -8,12 +8,34 @@ type InputProps = {
 };
 
 function Input(props: InputProps) {
+  const inputRef: React.MutableRefObject<null> = useRef(null);
+
   return (
     <div className="text-form-group">
       <label htmlFor="{props.id}">{props.labelName}</label>
-      <input id={props.id} type="text" placeholder={props.placeholder}></input>
+      <input
+        id={props.id}
+        type="text"
+        ref={inputRef}
+        placeholder={props.placeholder}
+      ></input>
     </div>
   );
 }
+
+// function Input(({ value, onChange }, ref) => {
+//   return (
+//     <div className="text-form-group">
+//       <label htmlFor="{props.id}">{props.labelName}</label>
+//       <input
+//         id={props.id}
+//         type="text"
+//         value={value}
+//         onChange={onChange}
+//         placeholder={props.placeholder}
+//       ></input>
+//     </div>
+//   );
+// })
 
 export default Input;
