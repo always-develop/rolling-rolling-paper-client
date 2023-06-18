b75de358da0f1f65283fa56e98a82e05454cb7c4
appleSimple <jy_4148@naver.com>
2023-06-18 17:41:55 +0900

    feat : implement input event

--- src/components/form/Input.tsx
+++ src/components/form/Input.tsx
@@ -1,41 +1,25 @@
-import React, { forwardRef, useRef } from 'react';
+import React, { LegacyRef, forwardRef } from 'react';
 import 'assets/styles/form/Input.scss';
 
 type InputProps = {
   id: string;
+  type: string;
   placeholder?: string;
   labelName?: string;
 };
 
-function Input(props: InputProps) {
-  const inputRef: React.MutableRefObject<null> = useRef(null);
-
+const Input = forwardRef(function Input(props: InputProps, ref: LegacyRef<HTMLInputElement>) {
   return (
     <div className="text-form-group">
       <label htmlFor="{props.id}">{props.labelName}</label>
       <input
         id={props.id}
-        type="text"
-        ref={inputRef}
+        type={props.type}
         placeholder={props.placeholder}
+        ref={ref}
       ></input>
     </div>
   );
-}
-
-// function Input(({ value, onChange }, ref) => {
-//   return (
-//     <div className="text-form-group">
-//       <label htmlFor="{props.id}">{props.labelName}</label>
-//       <input
-//         id={props.id}
-//         type="text"
-//         value={value}
-//         onChange={onChange}
-//         placeholder={props.placeholder}
-//       ></input>
-//     </div>
-//   );
-// })
+})
 
 export default Input;
