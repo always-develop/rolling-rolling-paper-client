2f0ef9041bb6ab83dff559d4502843ad984a3bba
appleSimple <jy_4148@naver.com>
2023-06-19 16:32:12 +0900

    feat : add signup component

---
+++ src/views/SignUp.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "assets/styles/views/SignUp.scss";
+
+function SignUp() {
+  return (
+    <div className="signup-wrapper">
+      닉네임 입력
+    </div>
+  );
+}
+
+export default SignUp;
