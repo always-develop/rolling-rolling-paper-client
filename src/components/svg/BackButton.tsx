2a6aeb53ffc61219c52ba9db939ed46d21489a2b
appleSimple <jy_4148@naver.com>
2023-07-10 23:26:13 +0900

    feat : add svg wrapper component

--- src/components/svg/BackButton.tsx
+++ src/components/svg/BackButton.tsx
@@ -1,16 +1,20 @@
+import SvgWrapper from './SvgWrapper';
+
 export default function BackButton() {
   return (
-    <svg
-      width="40"
-      height="40"
-      viewBox="0 0 40 40"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M28 19.0002H15.83L21.42 13.4102L20 12.0002L12 20.0002L20 28.0002L21.41 26.5902L15.83 21.0002H28V19.0002Z"
-        fill="#202020"
-      />
-    </svg>
+    <SvgWrapper>
+      <svg
+        width="40"
+        height="40"
+        viewBox="0 0 40 40"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M28 19.0002H15.83L21.42 13.4102L20 12.0002L12 20.0002L20 28.0002L21.41 26.5902L15.83 21.0002H28V19.0002Z"
+          fill="#202020"
+        />
+      </svg>
+    </SvgWrapper>
   );
 }
--- src/components/svg/Hamburger.tsx
+++ src/components/svg/Hamburger.tsx
@@ -1,33 +1,37 @@
+import SvgWrapper from './SvgWrapper';
+
 export default function Hamburger() {
   return (
-    <svg
-      width="38"
-      height="38"
-      viewBox="0 0 38 38"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M9 19.0002H29"
-        stroke="#202020"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M9 13.0002H29"
-        stroke="#202020"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M9 25.0002H29"
-        stroke="#202020"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
+    <SvgWrapper>
+      <svg
+        width="38"
+        height="38"
+        viewBox="0 0 38 38"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M9 19.0002H29"
+          stroke="#202020"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M9 13.0002H29"
+          stroke="#202020"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M9 25.0002H29"
+          stroke="#202020"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </SvgWrapper>
   );
 }
