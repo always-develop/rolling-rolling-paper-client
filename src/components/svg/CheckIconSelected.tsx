4c093b50fec697a8d12d2a92c55dabe11a404b9b
appleSimple <jy_4148@naver.com>
2023-07-18 21:53:55 +0900

    feat: add check svg component

---
+++ src/components/svg/CheckIconSelected.tsx
@@ -0,0 +1,29 @@
+export default function CheckIconSelected() {
+  return (
+    <svg
+      width="38"
+      height="39"
+      viewBox="0 0 38 39"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect x="2" y="2.00122" width="34" height="34" rx="17" fill="#70FF00" />
+      <path
+        d="M13 19.4059L17 23.4718L25 15.0012"
+        stroke="#434A50"
+        strokeWidth="2.5"
+        strokeLinecap="square"
+      />
+      <rect
+        x="1"
+        y="1.00122"
+        width="36"
+        height="36"
+        rx="18"
+        stroke="#70FF00"
+        strokeOpacity="0.28"
+        strokeWidth="2"
+      />
+    </svg>
+  );
+}
---
+++ src/components/svg/CheckIconUnselected copy.tsx
@@ -0,0 +1,29 @@
+export default function CheckIconUnselected() {
+  return (
+    <svg
+      width="38"
+      height="39"
+      viewBox="0 0 38 39"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect x="2" y="2.00122" width="34" height="34" rx="17" fill="#CED1D5" />
+      <path
+        d="M13 19.4059L17 23.4718L25 15.0012"
+        stroke="white"
+        strokeWidth="2.5"
+        strokeLinecap="square"
+      />
+      <rect
+        x="1"
+        y="1.00122"
+        width="36"
+        height="36"
+        rx="18"
+        stroke="#B2B7BC"
+        strokeOpacity="0.2"
+        strokeWidth="2"
+      />
+    </svg>
+  );
+}
